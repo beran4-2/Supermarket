15ce77760a183ff0d48c8b7052a9321cb2803291
beran4-2 <beran2@spsejecna.cz>
2026-05-31 01:12:59 +0200

    added functional info label

--- src/GUI/Windows/Screens/GameScreen.java
+++ src/GUI/Windows/Screens/GameScreen.java
@@ -4,6 +4,7 @@ import GUI.Customs.CustomButton;
 import GUI.Customs.CustomLabel;
 import GUI.Customs.CustomWindow;
 import javax.swing.*;
+import java.awt.*;
 
 public class GameScreen {
 
@@ -11,11 +12,20 @@ public class GameScreen {
     private JButton storageButton;
     private JButton shelvesButton;
     private JButton backButton;
+    private JButton continueButton;
+    private JButton restockButton;
+
+    private JLabel balanceLabel;
+    private JLabel dayLabel;
+    private JLabel activeCustomersLabel;
 
 
     public GameScreen(CustomWindow customWindow) {
         mainGameBackground = customWindow.paintBackground("/pictures/MainGameBackground/MainGameBackground.png");
         mainGameBackground.setLayout(null);
+
+        int monitorWidth = CustomWindow.getMonitorWidth();
+        int monitorHeight = CustomWindow.getMonitorHeight();
 
         int backButtonW = (int) (customWindow.getMonitorWidth() * 0.1822);
         int backButtonH = (int) (customWindow.getMonitorHeight() * 0.1638);
@@ -48,19 +58,64 @@ public class GameScreen {
         int restockButtonH = (int) (customWindow.getMonitorHeight() * 0.1481);
         int restockButtonX = (int) (customWindow.getMonitorWidth() * 0.8203);
         int restockButtonY = (int) (customWindow.getMonitorHeight() * 0.0556);
-        JButton restockButton = new CustomButton();
+        restockButton = new CustomButton();
         CustomButton.buttonImage(restockButton, "/pictures/MainGameBackground/MainGameRestockButton.png", restockButtonW, restockButtonH);
         restockButton.setLocation(restockButtonX, restockButtonY);
         mainGameBackground.add(restockButton);
 
-        int continueButtonW = (int) (customWindow.getMonitorWidth() * 0.2);
-        int continueButtonH = (int) (customWindow.getMonitorHeight() * 0.15);
-        int continueButtonX = (int) (customWindow.getMonitorWidth() * 0.7942);
-        int continueButtonY = (int) (customWindow.getMonitorHeight() * 0.8333);
-        JButton continueButton = new CustomButton();
+        int continueButtonW = (int) (customWindow.getMonitorWidth() * 0.25);
+        int continueButtonH = (int) (customWindow.getMonitorHeight() * 0.175);
+        int continueButtonX = (int) (customWindow.getMonitorWidth() * 0.745);
+        int continueButtonY = (int) (customWindow.getMonitorHeight() * 0.82);
+        continueButton = new CustomButton();
         CustomButton.buttonImage(continueButton, "/pictures/MainGameBackground/MainGameContinueButton.png", continueButtonW, continueButtonH);
         continueButton.setLocation(continueButtonX, continueButtonY);
         mainGameBackground.add(continueButton);
+
+        int infoTabW = (int)(monitorWidth * 0.25);
+        int infoTabH = (int)(monitorHeight * 0.30);
+        int infoTabX = (int)(monitorWidth * 0.745);
+        int infoTabY = (int)(monitorHeight * 0.559);
+
+        JLabel infoTabLabel = new JLabel();
+        GUI.Customs.CustomLabel.labelImage(infoTabLabel, "/informationTab.png", infoTabW, infoTabH);
+        infoTabLabel.setBounds(infoTabX, infoTabY, infoTabW, infoTabH);
+
+        infoTabLabel.setLayout(null);
+
+        Font labelFont = new Font("Arial", Font.BOLD, (int)(infoTabH * 0.08));
+
+        int textWidth = (int)(infoTabW * 0.8);
+        int textHeight = (int)(infoTabH * 0.15);
+
+        balanceLabel = new JLabel();
+        balanceLabel.setFont(labelFont);
+        balanceLabel.setForeground(Color.BLACK);
+        balanceLabel.setBounds(85, 85, textWidth, textHeight);
+        infoTabLabel.add(balanceLabel);
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(labelFont);
+        dayLabel.setForeground(Color.BLACK);
+        dayLabel.setBounds(85, 145, textWidth, textHeight);
+        infoTabLabel.add(dayLabel);
+
+        activeCustomersLabel = new JLabel();
+        activeCustomersLabel.setFont(labelFont);
+        activeCustomersLabel.setForeground(Color.BLACK);
+        activeCustomersLabel.setBounds(85, 200, textWidth, textHeight);
+        infoTabLabel.add(activeCustomersLabel);
+
+        mainGameBackground.add(infoTabLabel);
+
+
+
+    }
+
+    public void updateLabels(int balance, int day, int activeCustomers) {
+        balanceLabel.setText("$ " + balance);
+        dayLabel.setText("Day: " + day);
+        activeCustomersLabel.setText("Active Customers: " + activeCustomers);
     }
 
     public JPanel getMainGameBackground() {
@@ -69,4 +124,11 @@ public class GameScreen {
     public JButton getStorageButton() { return storageButton; }
     public JButton getShelvesButton() { return shelvesButton; }
     public JButton getBackButton() { return backButton; }
+    public JButton getContinueButton(){
+        return continueButton;
+    }
+
+    public JButton getRestockButton() {
+        return restockButton;
+    }
 }
